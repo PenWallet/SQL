91715ef05b44ca3aaa6b8405448003a644e08243
PenWallet <carterasindinero@gmail.com>
2017-11-06 14:42:37 +0100

    fyufgjh

---
+++ ayuntamientos.sql
@@ -0,0 +1,12 @@
+/*
+Persona (DNI[PK], Nombre, Apellidos, FechaDeAlta, Tipo)
+Alcalde (DNI[FK y PK], Partido, Nº votos)
+Secretario (DNI[FK y PK])
+Ayuntamiento (Dirección[PK], Nombre, Provincia)
+
+
+CREATE DATABASE ayuntamientoDB
+GO
+USE ayuntamientoDB
+GO
+CREATE TABLE Persona
