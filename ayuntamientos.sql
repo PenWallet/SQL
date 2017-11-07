311aba1f6d7ae9725192989a5450336758991b0a
PenWallet <carterasindinero@gmail.com>
2017-11-07 13:08:47 +0100

    SQL, Ayuntamientos

--- ayuntamientos.sql
+++ ayuntamientos.sql
@@ -1,12 +1,47 @@
 /*
-Persona (DNI[PK], Nombre, Apellidos, FechaDeAlta, Tipo)
-Alcalde (DNI[FK y PK], Partido, Nº votos)
-Secretario (DNI[FK y PK])
-Ayuntamiento (Dirección[PK], Nombre, Provincia)
+Personas (DNI[PK], Nombre, Apellidos, FechaDeAlta, Tipo)
+Alcaldes (DNI[FK y PK], Partido, Nº votos)
+Secretarios (DNI[FK y PK])
+Ayuntamientos (Dirección[PK], Nombre, Provincia)
 
+*/
 
 CREATE DATABASE ayuntamientoDB
 GO
 USE ayuntamientoDB
 GO
-CREATE TABLE Persona
+CREATE TABLE Personas (
+DNI char(9) Not null,
+Nombre varChar(20) Not null,
+Apellidos varChar (50) Not Null,
+FechaDeAlta date Not Null,
+Tipo varChar(15),
+Constraint PK_Personas Primary Key (DNI) )
+
+GO
+
+CREATE TABLE Alcaldes (
+DNI char(9) Not Null,
+Partido varChar(20) Not Null,
+NumeroDeVotos int Not Null,
+Constraint PK_Alcaldes Primary Key (DNI),
+Constraint FK_Alcaldes_Personas Foreign Key (DNI) REFERENCES Personas(DNI) ON DELETE NO ACTION ON UPDATE CASCADE )
+
+GO
+
+CREATE TABLE Secretarios (
+DNI char(9) Not Null,
+Constraint PK_Secretarios Primary Key (DNI),
+Constraint FK_Secretarios_Personas Foreign Key (DNI) REFERENCES Personas(DNI) ON DELETE NO ACTION ON UPDATE CASCADE )
+
+GO
+
+CREATE TABLE Ayuntamientos (
+Direccion char(40) Not Null,
+Nombre char(20) Not Null,
+Provincia char(20) Not Null,
+DNI_Alcaldes char(9) Not Null,
+DNI_Secretarios char(9) Not Null,
+Constraint PK_Ayuntamientos Primary Key (Direccion),
+Constraint FK_Ayuntamientos_Alcaldes Foreign Key (DNI_Alcaldes) REFERENCES Alcaldes(DNI),
+Constraint FK_Ayuntamientos_Secretarios Foreign Key (DNI_Secretarios) REFERENCES Secretarios(DNI) )
