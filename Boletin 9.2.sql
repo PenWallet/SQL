2298cbaa568e1e6d181fb6b48e5ce06c13da64ff
PenWallet <carterasindinero@gmail.com>
2018-02-12 01:23:04 +0100

    dfg

--- Boletin 9.2.sql
+++ Boletin 9.2.sql
@@ -41,6 +41,8 @@ SELECT E.LastName, E.FirstName, P.ProductName
 
 --5. Empleados que no han vendido nunca “Northwoods Cranberry Sauce” o
 --“Carnarvon Tigers”.
+SELECT FirstName, LastName From Employees
+EXCEPT
 SELECT E.FirstName, E.LastName
 	FROM Employees AS E
 		INNER JOIN Orders AS O
@@ -55,16 +57,50 @@ SELECT E.FirstName, E.LastName
 --6. Número de unidades de cada categoría de producto que ha vendido cada
 --empleado. Incluye el nombre y apellidos del empleado y el nombre de la
 --categoría.
+SELECT E.FirstName, E.LastName, C.CategoryName, SUM(OD.Quantity) AS [Cantidad vendida]
+	FROM Categories AS C
+		INNER JOIN Products AS P
+			ON C.CategoryID = P.CategoryID
+		INNER JOIN [Order Details] AS OD
+			ON P.ProductID = OD.ProductID
+		INNER JOIN Orders AS O
+			ON OD.OrderID = O.OrderID
+		INNER JOIN Employees AS E
+			ON O.EmployeeID = E.EmployeeID
+	GROUP BY E.FirstName, E.LastName, C.CategoryName
+	ORDER BY E.FirstName, E.LastName, C.CategoryName
 
 
 --7. Total de ventas (US$) de cada categoría en el año 97. Incluye el nombre de la
 --categoría.
-
+SELECT C.CategoryName, SUM(OD.UnitPrice * OD.Quantity) AS [Dinero total]
+	FROM Categories AS C
+		INNER JOIN Products AS P
+			ON C.CategoryID = P.CategoryID
+		INNER JOIN [Order Details] AS OD
+			ON P.ProductID = OD.ProductID
+		INNER JOIN Orders AS O
+			ON OD.OrderID = O.OrderID
+	WHERE year(O.OrderDate) = 1997
+	GROUP BY C.CategoryName
+	ORDER BY C.CategoryName
+	 
 
 --8. Productos que han comprado más de un cliente del mismo país, indicando el
 --nombre del producto, el país y el número de clientes distintos de ese país que
 --lo han comprado.
+SELECT * FROM Customers
 
+SELECT C.Country, COUNT(C.ContactName) AS [Numero de personas], P.ProductName
+	FROM Products AS P
+		INNER JOIN [Order Details] AS OD
+			ON P.ProductID = OD.ProductID
+		INNER JOIN Orders AS O
+			ON OD.OrderID = O.OrderID
+		INNER JOIN Customers AS C
+			ON O.CustomerID = C.CustomerID
+	GROUP BY C.Country, P.ProductName
+	HAVING COUNT(C.ContactName) > 1
 
 --9. Total de ventas (US$) en cada país cada año.
 
@@ -84,12 +120,59 @@ SELECT E.FirstName, E.LastName
 --nombre y apellidos del cliente y su dirección completa.
 
 
---14. Clientes que nos compran más de cinco productos diferentes.
+--14. Clientes que nos compran más de cinco productos diferentes. *
+GO
+CREATE VIEW Puntadelnabo2 AS
+SELECT C.ContactName, P.ProductName
+	FROM Products AS P
+		INNER JOIN [Order Details] AS OD
+			ON P.ProductID = OD.ProductID
+		INNER JOIN Orders AS O
+			ON OD.OrderID = O.OrderID
+		INNER JOIN Customers AS C
+			ON O.CustomerID = C.CustomerID
+GO
+SELECT ContactName, COUNT(*) AS [Numero de productos]
+	FROM Puntadelnabo2
+	GROUP BY ContactName
+	HAVING COUNT(ProductName) > 5
 
 
 --15. Vendedores (nombre y apellidos) que han vendido una mayor cantidad que la
 --media en US $ en el año 97.
 
+GO
+CREATE VIEW YerayCabron1 AS
+SELECT E.EmployeeID, SUM(OD.UnitPrice * OD.Quantity * (1-OD.Discount)) AS [Media US$]
+	FROM Employees AS E
+		INNER JOIN Orders AS O
+			ON E.EmployeeID = O.EmployeeID
+		INNER JOIN [Order Details] AS OD
+			ON O.OrderID = OD.OrderID
+	WHERE year(OrderDate) = 1997
+	GROUP BY E.EmployeeID
+GO
+
+SELECT YerayCabron1.EmployeeID, AVG(YerayCabron1.[Media US$])
+	FROM YerayCabron1
+	GROUP BY YerayCabron1.EmployeeID
+
+SELECT SUM(OD.UnitPrice * OD.Quantity * (1-OD.Discount))/COUNT(E.EmployeeID) AS [Media US$]
+	FROM Employees AS E
+		INNER JOIN Orders AS O
+			ON E.EmployeeID = O.EmployeeID
+		INNER JOIN [Order Details] AS OD
+			ON O.OrderID = OD.OrderID
+	WHERE year(OrderDate) = 1997
+
+SELECT E.LastName, E.FirstName, 
+	FROM Employees AS E
+		INNER JOIN Orders AS O
+			ON E.EmployeeID = O.EmployeeID
+		INNER JOIN [Order Details] AS OD
+			ON O.OrderID = OD.OrderID
+
 
 --16. Empleados que hayan aumentado su cifra de ventas más de un 10% entre dos
---años consecutivos, indicando el año en que se produjo el aumento
+--años consecutivos, indicando el año en que se produjo el aumento *
+
