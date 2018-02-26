e5885513c89411a57c37a95bddb0d6fb191160d8
PenWallet <carterasindinero@gmail.com>
2018-02-26 13:42:19 +0100

    FWEDF

--- Boletin Actualizaciones Northwind.sql
+++ Boletin Actualizaciones Northwind.sql
@@ -8,37 +8,71 @@ INSERT INTO Customers VALUES ('RAFMA', 'Buenas Tardes Co.', 'Rafael', 'Owner', '
 SELECT * FROM Orders
 SELECT * FROM [Order Details]
 SELECT * FROM Employees
+SELECT * FROM Customers
+SELECT * FROM Shippers
+SELECT * FROM Products
 
-INSERT INTO Orders (OrderID, CustomerID, EmployeeID, OrderDate, ShippedDate, ShipVia, Freight, ShipName, ShipAddress, ShipCity, ShipRegion, ShipPostalCode, ShipCountry)
-	VALUES (11078, 'RAFMA', 8, 
+INSERT INTO Orders (CustomerID, EmployeeID, OrderDate, ShippedDate, ShipVia, Freight, ShipName, ShipAddress, ShipCity, ShipRegion, ShipPostalCode, ShipCountry)
+	VALUES 
+	(
+		(SELECT CustomerID FROM Customers WHERE ContactName = 'Rafael'),
+		(SELECT EmployeeID FROM Employees WHERE LastName = 'Callahan' AND FirstName = 'Laura'),
+		(CURRENT_TIMESTAMP),
+		(CURRENT_TIMESTAMP),
+		(SELECT ShipperID FROM Shippers WHERE CompanyName = 'Speedy Express'),
+		(30),
+		(SELECT CompanyName FROM Shippers WHERE CompanyName = 'Speedy Express'),
+		(SELECT Address FROM Customers WHERE ContactName = 'Rafael'),
+		(SELECT City FROM Customers WHERE ContactName = 'Rafael'),
+		(SELECT Region FROM Customers WHERE ContactName = 'Rafael'),
+		(SELECT PostalCode FROM Customers WHERE ContactName = 'Rafael'),
+		(SELECT Country FROM Customers WHERE ContactName = 'Rafael')
+	)
+
+
 
 --Ante la bajada de ventas producida por la crisis, hemos de adaptar nuestros precios según las siguientes reglas:
---Los productos de la categoría de bebidas (Beverages) que cuesten más de $10 reducen su precio en un dólar.
---Los productos de la categoría Lácteos que cuesten más de $5 reducen su precio en un 10%.
---Los productos de los que se hayan vendido menos de 200 unidades en el último año, reducen su precio en un 5%
+	--Los productos de la categoría de bebidas (Beverages) que cuesten más de $10 reducen su precio en un dólar.
+	--Los productos de la categoría Lácteos que cuesten más de $5 reducen su precio en un 10%.
+	--Los productos de los que se hayan vendido menos de 200 unidades en el último año, reducen su precio en un 5%
+
+
+
 --Inserta un nuevo vendedor llamado Michael Trump. Asígnale los territorios de Louisville, Phoenix, Santa Cruz y Atlanta.
+
+
+
 --Haz que las ventas del año 97 de Robert King que haya hecho a clientes de los estados de California y Texas se le asignen al nuevo empleado.
+
+
+
 --Inserta un nuevo producto con los siguientes datos:
---ProductID: 90
---ProductName: Nesquick Power Max
---SupplierID: 12
---CategoryID: 3
---QuantityPerUnit: 10 x 300g
---UnitPrice: 2,40
---UnitsInStock: 38
---UnitsOnOrder: 0
---ReorderLevel: 0
---Discontinued: 0
---Inserta un nuevo producto con los siguientes datos:
---ProductID: 91
---ProductName: Mecca Cola
---SupplierID: 1
---CategoryID: 1
---QuantityPerUnit: 6 x 75 cl
---UnitPrice: 7,35
---UnitsInStock: 14
---UnitsOnOrder: 0
---ReorderLevel: 0
---Discontinued: 0
+	--ProductID: 90
+	--ProductName: Nesquick Power Max
+	--SupplierID: 12
+	--CategoryID: 3
+	--QuantityPerUnit: 10 x 300g
+	--UnitPrice: 2,40
+	--UnitsInStock: 38
+	--UnitsOnOrder: 0
+	--ReorderLevel: 0
+	--Discontinued: 0
+	--Inserta un nuevo producto con los siguientes datos:
+	--ProductID: 91
+	--ProductName: Mecca Cola
+	--SupplierID: 1
+	--CategoryID: 1
+	--QuantityPerUnit: 6 x 75 cl
+	--UnitPrice: 7,35
+	--UnitsInStock: 14
+	--UnitsOnOrder: 0
+	--ReorderLevel: 0
+	--Discontinued: 0
+
+
+
 --Todos los que han comprado "Outback Lager" han comprado cinco años después la misma cantidad de Mecca Cola al mismo vendedor
+
+
+
 --El pasado 20 de enero, Margaret Peacock consiguió vender una caja de Nesquick Power Max a todos los clientes que le habían comprado algo anteriormente. Los datos de envío (dirección, transportista, etc) son los mismos de alguna de sus ventas anteriores a ese cliente).
