8533fd4997892d3f33dbbb1117bc4aabbce06cfc
PenWallet <carterasindinero@gmail.com>
2018-05-07 14:36:08 +0200

    meh

--- Boletin 11.2.sql
+++ Boletin 11.2.sql
@@ -30,7 +30,7 @@ SELECT * FROM LTApuntes
 --Si la carrera no existe, si hay ocho caballos ya inscritos o si el caballo no existe o está ya inscrito en 
 --esa carrera, el parámetro de salida devolverá NULL.
 GO
-CREATE PROCEDURE InscribirCaballo (@IDCarrera int, @IDCaballo int, @NumeroAsignado int OUTPUT)
+ALTER PROCEDURE InscribirCaballo (@IDCarrera int, @IDCaballo int, @NumeroAsignado int OUTPUT)
 AS
 	BEGIN
 		IF(
@@ -40,15 +40,70 @@ AS
 			NOT EXISTS(SELECT IDCaballo FROM LTCaballosCarreras WHERE IDCaballo = @IDCaballo AND IDCarrera = @IDCarrera) 
 			)
 		BEGIN
-			DECLARE @Contador int = 1
-			WHILE(@Contador IN (SELECT Numero FROM LTCaballosCarreras WHERE IDCarrera = @IDCarrera))
+			SET @NumeroAsignado = 1
+			WHILE(@NumeroAsignado IN (SELECT Numero FROM LTCaballosCarreras WHERE IDCarrera = @IDCarrera))
 			BEGIN
-				SET @Contador = @Contador + 1
+				SET @NumeroAsignado = @NumeroAsignado + 1
 			END
 
 			INSERT INTO LTCaballosCarreras (IDCaballo, IDCarrera, Numero)
-									VALUES (@IDCaballo, @IDCarrera, @Contador)
+									VALUES (@IDCaballo, @IDCarrera, @NumeroAsignado)
 
 		END
+		ELSE
+		BEGIN
+			SET @NumeroAsignado = NULL
+			PRINT 'Hola Oscar'
+		END
+	END
+GO
+
+BEGIN TRANSACTION
+DECLARE @Salida int
+EXECUTE InscribirCaballo 3, 11, @Salida OUTPUT
+PRINT 'Salida: '+CAST(@Salida AS varchar)
+
+SELECT IDCarrera, IDCaballo, Numero FROM LTCaballosCarreras ORDER BY IDCarrera, IDCaballo
+--COMMIT
+--ROLLBACK
+
+
+
+--Añade a la tabla LTJugadores una nueva columna llamada LimiteCredito de tipo SmallMoney con el valor por defecto 50.
+--Este valor indicará el máximo saldo negativo que se permite al jugador. El saldo del jugador más el valor de esa
+--columna no puede ser nunca inferior a 0.
+--Escribe un procedimiento para grabar una apuesta. El procedimiento recibirá como parámetros el jugador, la carrera,
+--el caballo y el importe a apostar y devolverá con return un código de terminación según la siguiente tabla:
+
+BEGIN TRANSACTION
+ALTER TABLE LTJugadores
+	ADD LimiteCredito smallmoney NOT NULL
+		CONSTRAINT LTJ_LC_DefaultValue DEFAULT 50
+COMMIT
+
+GO
+CREATE PROCEDURE GrabarApuesta (@IDJugador int, @IDCarrera int, @IDCaballo int, @Apuesta smallmoney, @Codigo int OUTPUT)
+AS
+	BEGIN
+		IF(NOT EXISTS(SELECT ID FROM LTCarreras WHERE ID = @IDCarrera))
+			SET @Codigo = 2
+		ELSE
+			BEGIN
+				IF( (SELECT Fecha FROM LTCarreras WHERE ID = @IDCarrera) < CAST(GETDATE() AS date) )
+					SET @Codigo = 3
+				ELSE
+					BEGIN
+						IF( @IDCaballo != (SELECT IDCaballo FROM LTCaballosCarreras WHERE IDCarrera = @IDCarrera AND IDCaballo = @IDCaballo) )
+							SET @Codigo = 5
+						ELSE
+							BEGIN
+								IF( @Apuesta > (SELECT --¿MAX(saldo)?
+													FROM LTJugadores AS J
+														INNER JOIN LTApuntes as A
+															ON J.ID = A.IDJugador) )
+							END
+					END
+
+			END
 	END
 GO
